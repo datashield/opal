0fc2edebebf5b560b8d4653c014242d68404df9f
Patricia Ryser-Welch <45422782+patRyserWelch8@users.noreply.github.com>
2019-04-04 10:40:29 +0100

    http to https

--- R/datashield.buildLoginToDataShield.o.r
+++ R/datashield.buildLoginToDataShield.o.r
@@ -8,29 +8,22 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #-------------------------------------------------------------------------------
 
-#'@title Builds a dataframe to login to datashield
+#'@title Builds a datagframe to login to datashield
 #'@description This function generates a valid data frame, that can be used to login
 #'to some data computers (i.e. opal servers). The data frame models a double-entry table. The
 #'columns are defined as server, url, user, password and table name. Each row holds the information 
 #'in relation to one data computer. The values for each column are passed  to the function as arguments.   
-#'
 #'@param data.computers.name A vector of characters listing all the names of the data computers
 #'@param data.computers.url A vector of characters listing each data computer HTTP address. The format is http://[TCPIP address or host name][:port]
 #'@param data.computers.table.name A vector of characters listing the name of the table stored in a data computer 
 #'@param users.id A vector of characters listing a valid user name to log on on each server.
 #'@param users.password A vector of characters listing the password for each user to log in to a data computer.
-#'@return a data frame formatter in this manner: (server,url,user,password,table). If the arguments are not correct. Then a data.frame with no rows is created.
+#'@return a data frame formatter in this manner: (server,url,user,password,table)
 #'
-#'The expactactions are as follow:
-#'Expectation no 0: the return value is a data.frame 
-#'Expectation no 1: the number of columns is equal 5. 
-#'Expectation no 2: the number of rows is equal to the number of servers
-#'Expectation no 3: the number of rows is equal to 0, if the length of url, user, or table is smaller than the length of server
-#'Expectation no 4: the number of row is 0, if any of the urls does not start with http
-#'@author Patricia Ryser-Welch
-
-
-datashield.build.login.data.frame.o <- function (data.computers.name, data.computers.url, data.computers.table.name,  users.id, users.password) 
+#'
+#'
+#'
+datashield.build.Login.Data.frame.o <- function (data.computers.name, data.computers.url, data.computers.table.name,  users.id, users.password) 
 {
   #assign the arguments to the data frame format.
   server <- as.character(data.computers.name)
@@ -38,23 +31,26 @@ datashield.build.login.data.frame.o <- function (data.computers.name, data.compu
   user <- as.character(users.id)
   password <- as.character(users.password)
   table <- as.character(data.computers.table.name)
-  return.data.frame  =  data.frame(server = character(0), user = character(0),user= character(0), password = character(0),table=character(0))
-  colnames(return.data.frame) <- c('server','url','user','password','table')
   NO.COLUMNS = 5
   
   #Verify the length of each vector is the same
   expected.elements = length(server) * NO.COLUMNS
   total.elements = length(server) + length(url) + length(user) + length(password) + length(table)
   
-  
-  if (expected.elements == total.elements)
+  if (expected.elements != total.elements)
   {
-    #verifies the url stars with http
-    if (all(startsWith(url,"https")))
+    stop("The length of the vectors passed as arguments are not the same length.")
+  }
+  else 
+  {
+    if (all (startsWith(url,"https")))
     {
-      return.data.frame  = data.frame(server,url,user,password,table)
+      return(data.frame(server,url,user,password,table))
+    }
+    else 
+    {
+      warning("Each url must starts https")
+      return(data.frame())
     }
   }
-  return(return.data.frame)
 }
-
